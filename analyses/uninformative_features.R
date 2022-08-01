f0b3e6438b0724a633a8a5b3d38dabaecaaa7585
LeoGrin <leo.grinsztajn@polytechnique.edu>
2022-08-01 14:39:15 -0700

    add repositories for data download

--- analyses/uninformative_features.R
+++ analyses/uninformative_features.R
@@ -216,3 +216,81 @@ ggplot() +
   theme(legend.position="none")
 
 ggsave("analyses/plots/removed_features.jpg", width=16, height=9)
+
+
+res_ %>%
+  select(-max_test_score, -min_test_score) %>% 
+  slice_tail(n=1)
+
+# ggplot() +
+#   geom_line(aes(x = random_rank, y = mean_test_score, color=model_name, group=model_name), data=res_, size=2) +
+#   geom_dl(aes(label = model_name, x=random_rank, y=mean_test_score, color=model_name), method = list(dl.combine("smart.grid"), cex=1.3), data=res_)  +
+#   #geom_hline(aes(yintercept=mean_test_score, color=model_name), data=res_ %>% filter(max_rank == 0), linetype="dotted", size=1.5) +
+#   #geom_line(aes(x = log_cum_time_factor, y = max_test_score, color=model_name, group=model_name)) +
+#   #geom_line(aes(x = log_cum_time_factor, y = min_test_score, color=model_name, group=model_name)) +
+#   geom_ribbon(aes(x = random_rank, ymin=min_test_score, ymax=max_test_score, fill=model_name), data=res_, alpha=0.2) +
+#   #geom_point(aes(x = cum_time, y = mean_test_score, color=model_name, group=model_name)) +
+#   #scale_x_log10(limits= c(10, 1E5)) +
+#   scale_x_log10() +
+#   #ylim(0.977, 0.993)+
+#   xlab("Random search time (seconds)") +
+#   ylab("Normalized test score of best model \n (on valid set) up to this time") +
+#   theme_minimal(base_size=22) +
+#   theme(legend.position="none")
+
+
+res_time_ <- res_time %>% #select(model_name, data__keyword, log_cum_time_factor,log_cum_time, mean_val_score,best_val_score_so_far, mean_test_score) %>% 
+  group_by(model_name, cum_time_factor) %>% 
+  summarise(count=sum(!is.na(mean_test_score)),
+            max_test_score = mean(mean_test_score, na.rm=T) + 2 * sd(mean_test_score, na.rm=T) / sqrt(count),
+            min_test_score = mean(mean_test_score, na.rm=T) - 2 * sd(mean_test_score, na.rm=T) / sqrt(count),
+            mean_test_score = mean(mean_test_score, na.rm=T),
+            cum_time = mean(cum_time))
+
+
+library(directlabels)
+
+ggplot() +
+  geom_line(aes(x = cum_time, y = mean_test_score, color=model_name, group=model_name), data=res_time_, size=2) +
+  geom_dl(aes(label = model_name, x=cum_time, y=mean_test_score, color=model_name), method = list(dl.combine("smart.grid"), cex=1.3), data=res_time_)  +
+  #geom_hline(aes(yintercept=mean_test_score, color=model_name), data=res_ %>% filter(max_rank == 0), linetype="dotted", size=1.5) +
+  #geom_line(aes(x = log_cum_time_factor, y = max_test_score, color=model_name, group=model_name)) +
+  #geom_line(aes(x = log_cum_time_factor, y = min_test_score, color=model_name, group=model_name)) +
+  geom_ribbon(aes(x = cum_time, ymin=min_test_score, ymax=max_test_score, fill=model_name), data=res_time_, alpha=0.2) +
+  #geom_point(aes(x = cum_time, y = mean_test_score, color=model_name, group=model_name)) +
+  scale_x_log10(limits= c(10, 1E5)) +
+  #ylim(0.977, 0.993)+
+  xlab("Random search time (seconds)") +
+  ylab("Normalized test score of best model \n (on valid set) up to this time") +
+  theme_minimal(base_size=22) +
+  theme(legend.position="none")# +
+#scale_x_discrete(breaks=c(10, 60, 3600, 3600*24), labels=c("10 seconds", "1 minute", "1 hour", "1 day"))
+
+ggsave("analyses/plots/random_search_time.jpg", width=16, height=9)
+
+
+# Iters
+
+#TODO: think per dataset?
+res_ <- res %>% 
+  group_by(model_name, max_rank) %>% 
+  summarise(max_test_score = mean(mean_test_score) + 2 * sd(mean_test_score, na.rm=T) / sqrt(N_SHUFFLE),
+            min_test_score = mean(mean_test_score) - 2 * sd(mean_test_score, na.rm=T) / sqrt(N_SHUFFLE),
+            mean_test_score = mean(mean_test_score)) 
+ggplot() +
+  geom_hline(aes(yintercept=mean_test_score, color=model_name), data=res_ %>% filter(max_rank == 0), linetype="dotted", size=1.5) +
+  geom_line(aes(x = max_rank, y = mean_test_score, color = model_name), data=res_, size=2) +
+  geom_ribbon(aes(x=max_rank, ymin = min_test_score, ymax=max_test_score, fill=model_name), data=res_, alpha=0.3) +
+  geom_text(aes(x=150, y=mean_test_score + 0.0004, color=model_name, label=str_c(model_name, " (Default)")), data=res_ %>% filter(max_rank == 0), size=7) +
+  scale_x_log10() +
+  xlab("Number of random search iterations") +
+  ylab("Normalized test score of best model \n (on valid set) up to this iteration") +
+  theme_minimal(base_size=22) +
+  theme(legend.position="none")
+
+
+
+
+
+
+
