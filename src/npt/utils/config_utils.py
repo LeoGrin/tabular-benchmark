55f166fdf92f3e0d45cc407c7fb86468710bf6f2
LeoGrin <leo.grinsztajn@polytechnique.edu>
2022-09-02 16:08:33 +0100

    cleaning + requirements

--- src/npt/utils/config_utils.py
+++
@@ -1,9 +0,0 @@
-class Args:
-    def __init__(self, data: dict = None):
-        if data is None:
-            self.data = {}
-        else:
-            self.data = data
-
-    def __getattr__(self, key):
-        return self.data.get(key, None)
